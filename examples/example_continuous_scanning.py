aa7ba4f3d5b416b3e3fa0f74eac9545323ab1aa1
Arend de Jonge <arenddejonge@gmail.com>
2021-01-13 17:42:08 +0100

    fix example_continuous_scanning

--- examples/example_continuous_scanning.py
+++ examples/example_continuous_scanning.py
@@ -11,20 +11,24 @@ from crownstone_ble import CrownstoneBle
 
 
 def showNewData(data):
-	print("New data received!")
-	print(json.dumps(data, indent=2))
-	print("-------------------")
-
+    print("New data received!")
+    print(json.dumps(data, indent=2))
+    print("-------------------")
 
 
 # Initialize the Bluetooth Core.
 # Fill in the correct hciIndex, see the readme.
 # Fill in the correct keys, see the readme.
 core = CrownstoneBle(hciIndex=0)
-print("We're loading some default encryption keys into the library: \"adminKeyForCrown\", \"memberKeyForHome\", \"basicKeyForOther\", \"MyServiceDataKey\", \"aLocalizationKey\", \"MyGoodMeshAppKey\", \"MyGoodMeshNetKey\".\n")
-core.setSettings("adminKeyForCrown", "memberKeyForHome", "basicKeyForOther", "MyServiceDataKey", "aLocalizationKey", "MyGoodMeshAppKey", "MyGoodMeshNetKey")
 
+keys = ["adminKeyForCrown", "memberKeyForHome", "basicKeyForOther", "MyServiceDataKey", "aLocalizationKey",
+        "MyGoodMeshAppKey", "MyGoodMeshNetKey"]
+print("We're loading some default encryption keys into the library:", ", ".join(keys))
+core.setSettings(*keys)
 
-print("Scanning for Crownstones..")
-core.startScanning(60)
+for i in range(20):
+	# note: cannot use large values for startScanning. See doc.
+	print("Scanning for Crownstones..")
+	core.startScanning(3)
+
 core.shutDown()
