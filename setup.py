f212c732de6fb7948ba52d17ad71483cfad1e630
vliedel <bart@crownstone.rocks>
2021-03-09 15:32:45 +0100

    Revert "version bump for release 1.0.1"
    This reverts commit e76cd43c6bc8a4c20bc75bf028e3455bfd28ac80.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='crownstone-ble',
-    ersion="1.0.1",
+    version='1.0.0',
     packages=find_packages(exclude=["examples","testing"]),
     author="Crownstone B.V.",
     long_description=long_description,
