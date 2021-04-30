150928bcd90fe9945e1910a2ccdf30b38f7ad701
alexdm0 <alexdemulder@gmail.com>
2021-04-30 12:51:55 +0200

    marked the master version as unreleased

--- crownstone_ble/core/CrownstoneBle.py
+++ crownstone_ble/core/CrownstoneBle.py
@@ -24,7 +24,7 @@ from crownstone_ble.core.modules.RssiChecker import RssiChecker
 _LOGGER = logging.getLogger(__name__)
 
 class CrownstoneBle:
-    __version__ = "2.1.0"
+    __version__ = "x.x.x--UNRELEASED"
     
     def __init__(self, bleAdapterAddress: str = None):
         # bleAdapterAddress is the MAC address of the adapter you want to use.
