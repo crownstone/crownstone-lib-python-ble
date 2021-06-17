4a453be0d6524dec50af4f23017987e4bf49b6f8
vliedel <bart@crownstone.rocks>
2021-06-17 17:07:45 +0200

    Added setState().

--- crownstone_ble/core/ble_modules/StateHandler.py
+++ crownstone_ble/core/ble_modules/StateHandler.py
@@ -1,15 +1,17 @@
 from crownstone_core import Conversion
 from crownstone_core.Exceptions import CrownstoneError, CrownstoneException
 from crownstone_core.packets.ResultPacket import ResultPacket
-from crownstone_core.protocol.BlePackets import ControlStateGetPacket
-from crownstone_core.protocol.BluenetTypes import StateType
+from crownstone_core.protocol.BlePackets import ControlStateGetPacket, ControlStateSetPacket
+from crownstone_core.protocol.BluenetTypes import StateType, ResultValue
 from crownstone_core.protocol.Characteristics import CrownstoneCharacteristics
 from crownstone_core.protocol.Services import CSServices
 from crownstone_core.protocol.SwitchState import SwitchState
 
+from crownstone_ble import CrownstoneBle
+
 
 class StateHandler:
-    def __init__(self, bluetoothCore):
+    def __init__(self, bluetoothCore: CrownstoneBle):
         self.core = bluetoothCore
         
     async def getSwitchState(self) -> SwitchState:
@@ -24,7 +26,12 @@ class StateHandler:
         # TODO: check result code
         bytesResult = await self._getState(StateType.TIME)
         return Conversion.uint8_array_to_uint32(bytesResult)
-    
+
+    async def getDimmingAllowed(self) -> bool:
+        stateVal = await self._getState(StateType.PWM_ALLOWED)
+        # TODO: convert to uint8?
+        return stateVal != 0
+
     
     
     
@@ -33,23 +40,23 @@ class StateHandler:
     """
     
     
-    async def _getState(self, stateType):
+    async def _getState(self, stateType) -> list:
         """
         :param stateType: StateType
         """
         result = await self.core.ble.setupSingleNotification(CSServices.CrownstoneService, CrownstoneCharacteristics.Result, lambda: self._requestState(stateType))
 
         resultPacket = ResultPacket(result)
-        if resultPacket.valid:
-            # the payload of the resultPacket is padded with stateType and ID at the beginning
-
-            state = []
-            for i in range(6, len(resultPacket.payload)):
-                state.append(resultPacket.payload[i])
-
-            return state
-        else:
+        if not resultPacket.valid:
             raise CrownstoneException(CrownstoneError.INCORRECT_RESPONSE_LENGTH, "Result is invalid")
+
+        # The payload of the resultPacket is padded with stateType and ID at the beginning
+        state = []
+        for i in range(6, len(resultPacket.payload)):
+            state.append(resultPacket.payload[i])
+
+        return state
+
 
     async def _requestState(self, stateType):
         await self.core.ble.writeToCharacteristic(
@@ -57,3 +64,14 @@ class StateHandler:
             CrownstoneCharacteristics.Control,
             ControlStateGetPacket(stateType).getPacket()
         )
+
+    async def _setState(self, packet: ControlStateSetPacket):
+        result = await self.core.ble.setupSingleNotification(CSServices.CrownstoneService, CrownstoneCharacteristics.Result, lambda: self._setStateCommand(packet))
+        resultPacket = ResultPacket(result)
+        if not resultPacket.valid:
+            raise CrownstoneException(CrownstoneError.INCORRECT_RESPONSE_LENGTH, "Result is invalid")
+        if resultPacket.resultCode not in [ResultValue.SUCCESS, ResultValue.SUCCESS_NO_CHANGE]:
+            raise CrownstoneException(CrownstoneError.RESULT_NOT_SUCCESS, f"Result code is {resultPacket.resultCode}")
+
+    async def _setStateCommand(self, packet: ControlStateSetPacket):
+        await self.core.control._writeControlPacket(packet.getPacket())
