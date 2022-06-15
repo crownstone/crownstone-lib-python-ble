c843b1d9e890fb645b4c0b08c1cef2e1355b6a69
vliedel <bart@crownstone.rocks>
2022-06-15 11:42:13 +0200

    Mark non-release again.

--- crownstone_ble/core/CrownstoneBle.py
+++ crownstone_ble/core/CrownstoneBle.py
@@ -26,7 +26,7 @@ from crownstone_ble.core.modules.RssiChecker import RssiChecker
 _LOGGER = logging.getLogger(__name__)
 
 class CrownstoneBle:
-    __version__ = "2.4.0"
+    __version__ = "2.4.0-git"
     
     def __init__(self, bleAdapterAddress: str = None):
         # bleAdapterAddress is the MAC address of the adapter you want to use.
